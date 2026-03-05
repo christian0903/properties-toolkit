b5d9e2dc403d767474c03bbd17b714e48266fefe
christian0903 <Gi)jsdmf(007>
2026-03-05 21:04:22 +0100

    2026-03-05-03

--- src/modals/search-replace-modal.ts
+++ src/modals/search-replace-modal.ts
@@ -34,7 +34,7 @@ export class SearchReplaceModal extends Modal {
 			.setName('Property name')
 			.setDesc('The property to search in')
 			.addText(text => text
-				.setPlaceholder('E.G. Status, type, category')
+				.setPlaceholder('e.g. status, type, category')
 				.onChange(value => {
 					this.params.propertyName = value.trim();
 				}));
